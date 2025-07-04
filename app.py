b12117434447d370154293d1bf99606ba342c3a6
shivam9906 <shivkumarele99@gmail.com>
2025-07-04 12:31:34 +0530

    workflow2

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return "Hello from ECS! 🚀"
 
+def home2():
+    return "Hello from TESTING! 🚀"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
